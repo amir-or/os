1614088fa328653eecabe19bfbd51d7faced9fb8
einav13 <einav.avital1@mail.huji.ac.il>
2025-06-08 13:26:02 +0300

    the rest ex4

---
+++ ex/ex4/Test/test1_write_read_all_virtual_memory.cpp
@@ -0,0 +1,24 @@
+#include "../Resources/VirtualMemory.h"
+#include "../Resources/PhysicalMemory.h"
+#include <math.h>
+
+#include <cstdio>
+#include <cassert>
+#include <iostream>
+
+int main(int argc, char **argv) {
+    VMinitialize();
+    for (uint64_t i = 0; i < VIRTUAL_MEMORY_SIZE; ++i) {
+        VMwrite(i, i);
+    }
+
+    for (uint64_t i = 0; i < VIRTUAL_MEMORY_SIZE; ++i) {
+        word_t value;
+        VMread(i, &value);
+        assert(uint64_t(value) == i);
+    }
+
+    printf("success\n");
+
+    return 0;
+}
